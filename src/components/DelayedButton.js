4f526bd2978eec51b00f3b43b0ef389ea669ac78
Lena Hou <lenahou2013@gmail.com>
2021-11-17 18:50:00 -0500

    commit tree efcb7881c0ad4e64c8e8c4409528f2b2f54a5eb8

--- src/components/DelayedButton.js
+++ src/components/DelayedButton.js
@@ -1,1 +1,22 @@
 // Code DelayedButton Component Here
+
+import React from 'react'
+
+class DelayedButton extends React.Component{
+
+    // By the time the setTimeout fires, the event object will have already been returned to the pool. 
+    // We can fix this by calling e.persist()
+    handleClick = (e) => {
+        e.persist();
+        setTimeout(() => {this.props.onDelayedClick(e)}, this.props.delay)
+        }
+        
+
+    render(){
+        return(
+            <button onClick={this.handleClick}>Delayed</button>
+        )
+    }
+}
+
+export default DelayedButton
